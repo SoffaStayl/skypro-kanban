3b5ea77f207197b6db4ff9af416a6fd62773bb97
SoffaStayl <moiseenko01@mail.ru>
2024-04-22 19:18:33 +0700

    l

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,52 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
+import PopBrowse from "./components/popups/PopBrowse";
+import PopExit from "./components/popups/PopExit";
+import PopNewCard from "./components/popups/PopNewCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className="wrapper">
+        {/* pop-up start */}
+        <PopExit />
+        <PopNewCard />
+        <PopBrowse />
+        {/* pop-up end */}
+        <Header />
+        <Main />
       </div>
-      function App() {
-  // Задаем имя пользователя, которое будет передано в компонент WelcomeMessage
-  const userName = 'Алексей';
-
-  // Рендерим компонент WelcomeMessage и передаем в него проп name со значением переменной userName
-  // Если userName не было бы определено, то можно было бы не передавать проп name, и в сообщении по умолчанию использовалось бы "пользователь"
-  return (
-    <>
-      <WelcomeMessage name={userName} />
+      <script src="js/script.js"></script>
     </>
-  );
-}
-
-export default App;
-      <h1>Vite + React</h1>
-     
-   
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+       );
+      }
+      export default App;
